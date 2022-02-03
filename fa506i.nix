a719aca572f51625aa3cdc95dcc3e02ea7e8f03d
Haemin Yoo <haemin@zzz.pe.kr>
2022-02-03 23:29:06 +0900

    Setup xmonad

--- fa506i.nix
+++ fa506i.nix
@@ -64,22 +64,91 @@ in
     boot.blacklistedKernelModules = [ "nouveau" "nvidia_drm" "nvidia_modeset" ]; #"nvidia"
     environment.systemPackages = [ pkgs.linuxPackages.nvidia_x11 nvidia-offload ];
 
-
-    # Option "PrimaryGPU" "Yes"
-    #services.xserver.config = lib.mkAfter ''
-    #  Driver "modesetting"
-    #'';
-
     services.xserver = {
       displayManager = {
         gdm.enable = true;
         gdm.wayland = true;
         gdm.nvidiaWayland = true;
+        sessionCommands = ''
+          xrandr --setmonitor HDMI-A-0~1 2560/593x1440/334+1920+0 HDMI-A-0
+          xrandr --setmonitor HDMI-A-0~2 880/204x1440/334+4480+0 none
+        '';
       };
       videoDrivers = [ "modeset" "nvidia" ];
+
       desktopManager = {
-        mate.enable = lib.mkForce true;
+        mate.enable = lib.mkForce false;
         gnome.enable = false;
+        lxqt.enable = true;
+      };
+
+      windowManager.xmonad = {
+        enable = true;
+        enableContribAndExtras = true;
+        config = ''
+          --
+          -- Configuration file for XMonad + MATE
+          --
+          --  Usage:
+          --      * Copy this file to ~/.xmonad/
+          --      * Run:    $ xmonad --recompile
+          --      * Launch: $ xmonad --replace
+          --      [Optional] Create an autostart to command with "xmonad --replace"
+          --
+          --  Author: Arturo Fernandez <arturo at bsnux dot com>
+          --  Inspired by:
+          --      Spencer Janssen <spencerjanssen@gmail.com>
+          --      rfc <reuben.fletchercostin@gmail.com>
+          --  License: BSD
+          --
+          import XMonad
+          import XMonad.Config.Desktop
+          import XMonad.Util.Run (safeSpawn)
+          import qualified Data.Map as M
+          import System.Environment (getEnvironment)
+          import XMonad.Util.EZConfig
+          import XMonad.Hooks.SetWMName
+          import XMonad.Layout.LayoutScreens
+          import XMonad.Layout.TwoPane
+          
+          mateConfig = desktopConfig
+              { terminal    = "alacritty"
+              , keys        = mateKeys <+> keys desktopConfig
+              -- , startupHook = setWMName "LG3D"
+              }
+          
+          mateKeys (XConfig {modMask = modm}) = M.fromList $
+              [ ((modm, xK_p), mateRun)
+              , ((modm .|. shiftMask, xK_q), spawn "mate-session-save --kill") ]
+          
+          mateRun :: X ()
+          mateRun = withDisplay $ \dpy -> do
+              rw <- asks theRoot
+              mate_panel <- getAtom "_MATE_PANEL_ACTION"
+              panel_run   <- getAtom "_MATE_PANEL_ACTION_RUN_DIALOG"
+          
+              io $ allocaXEvent $ \e -> do
+                  setEventType e clientMessage
+                  setClientMessageEvent e rw mate_panel 32 panel_run 0
+                  sendEvent dpy rw False structureNotifyMask e
+                  sync dpy False
+          
+          main = do
+              xmonad $ mateConfig
+                          { modMask = mod4Mask
+                           , borderWidth = 4
+                           , focusedBorderColor = "#7FBC71"
+                          } `additionalKeysP` myKeys
+          
+          myKeys = [  (("M4-f"), spawn "firefox")
+                     ,(("M4-a"), spawn "emacs")
+                     -- ,("M4--", layoutSplitScreen 2 (TwoPane 0.74418604651 0.25581395349))
+                     -- ,("M4-=", rescreen)
+                     -- ,(("M4--"), spawn "xrandr -s 1280x800")
+                     -- ,(("M4-="), spawn "xrandr -s 1920x1080")
+                     ,(("M4-z"), kill)
+                   ]
+        '';
       };
     };
 

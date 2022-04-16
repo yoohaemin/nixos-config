7268464e52e4f4d518106699786079c193803fd1
Haemin Yoo <haemin@zzz.pe.kr>
2022-04-16 14:03:18 +0900

    Enable gdm for 15zd980

--- 15z980.nix
+++ 15z980.nix
@@ -35,6 +35,7 @@
       brightnessctl
     ];
 
+    services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
     services.xserver.desktopManager.gnome.flashback.enableMetacity = true;
   };
